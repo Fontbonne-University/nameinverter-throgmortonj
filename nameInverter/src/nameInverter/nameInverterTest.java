da9019e1997164363a441aef228aeae7d621e1ee
zanwar79 <zahidsbusinessaccount@gmail.com>
2019-03-24 13:08:06 -0500

    Initial Upload

---
+++ nameInverter/src/nameInverter/nameInverterTest.java
@@ -0,0 +1,19 @@
+package nameInverter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class nameInverterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void nothing() {
+
+	}
+
+}
